9da9198717a2a3391aca683bfefcf88c6f462df3
rlorimier <rere.rnt@gmail.com>
2022-10-02 15:06:48 +0000

    started allauth set up

--- blog/views.py
+++ blog/views.py
@@ -5,20 +5,27 @@ from .models import Post
 
 class PostList(generic.ListView):
     model = Post
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'index.html'
+    queryset = Post.objects.filter(status=1).order_by("-created_on")
+    template_name = "index.html"
     paginate_by = 6
 
 
 class PostDetail(View):
 
-    def get(seft, request, slug, *args, **kwargs):
-        queryset = Post.abjects.filter(status=1)
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filter(approved=True).order_by('created_on')
+        comments = post.comments.filter(approved=True).order_by("-created_on")
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
 
-        return render(request, 'post_details.html',
-            {'post': post, 'comments': comments, 'liked': liked})
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked
+            },
+        )
