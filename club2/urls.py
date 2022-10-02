cdfa207a492ec0fb0100fff87be7441fd6bf38cc
rlorimier <rere.rnt@gmail.com>
2022-10-02 17:14:41 +0000

    created about html

--- club2/urls.py
+++ club2/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('summernore/', include('django_summernote.urls')),
+    path('summernote/', include('django_summernote.urls')),
     path('', include('blog.urls'), name='blog-urls'),
     path('accounts/', include('allauth.urls')),
 ]
