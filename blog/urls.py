6bc014548c25e563be0bf2fd94841f5672f5815a
rlorimier <rere.rnt@gmail.com>
2022-10-05 14:35:55 +0000

    included about us page

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='index'),
+    path('about/', views.about, name='about'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -77,3 +77,8 @@ class PostLike(View):
             post.likes.add(request.user)
 
         return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
+
+def about(request):
+    aboutus = ""
+    return render(request, 'about.html', {'aboutus': aboutus})
